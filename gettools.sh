575fb39d38b17bfff8b6fdb2e66ce3dbdfabe603
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-12-21 10:42:35 +0100

    Linux 4.9.207

---
+++ gettools.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
+# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
+# Android Kernel Build Script
+
+# Install build package for debian based linux
+sudo apt install bc bash git-core gnupg build-essential \
+    zip curl make automake autogen autoconf autotools-dev libtool shtool python \
+    m4 gcc libtool zlib1g-dev flex bison libssl-dev
+
+# Clone toolchain
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r20 --depth=1 stock
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r20 --depth=1 stock_32
+
+# Clone AnyKernel3
+git clone https://github.com/rama982/AnyKernel3 -b onc-miui
+
+#Download Clang
+if [ ! -d clang ]; then
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-10.0.0_r20/clang-r353983c.tar.gz
+    mkdir -p clang/clang-r353983c/
+    tar xvzf clang-r353983c.tar.gz -C clang/clang-r353983c
+    rm clang-r353983c.tar.gz
+fi
