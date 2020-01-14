3770b65c8486f54836ccfed357deddb623c66d86
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2020-01-14 20:04:32 +0100

    Linux 4.9.210

--- gettools.sh
+++ gettools.sh
@@ -14,7 +14,7 @@ git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarc
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r20 --depth=1 stock_32
 
 # Clone AnyKernel3
-git clone https://github.com/rama982/AnyKernel3 -b onc-miui
+git clone https://github.com/mylove90/AnyKernel3 -b master
 
 #Download Clang
 if [ ! -d clang ]; then
