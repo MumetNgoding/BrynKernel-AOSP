c50ed48a285309d17aabda0ff1a430e02f8d73f0
BryanHafidz <nugroho31.com@gmail.com>
2020-08-07 13:41:14 +0700

    Update gettools.sh

--- gettools.sh
+++ gettools.sh
@@ -10,8 +10,8 @@ sudo apt install bc bash git-core gnupg build-essential \
     m4 gcc libtool zlib1g-dev flex bison libssl-dev
 
 # Clone toolchain
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r35 --depth=1 stock
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r35 --depth=1 stock_32
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r41 --depth=1 stock
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r41 --depth=1 stock_32
 
 # Clone AnyKernel3
 git clone https://github.com/MumetNgoding/AnyKernel3.git -b master
@@ -19,8 +19,8 @@ git clone https://github.com/MumetNgoding/AnyKernel3.git -b master
 #Download Clang
 if [ ! -d clang ]; then
     git clone https://github.com/silont-project/silont-clang.git --depth=1
-    git clone https://github.com/arter97/arm64-gcc.git --depth=1
-    git clone https://github.com/arter97/arm32-gcc.git --depth=1
+#    git clone https://github.com/arter97/arm64-gcc.git --depth=1
+#    git clone https://github.com/arter97/arm32-gcc.git --depth=1
 #    git clone https://github.com/MumetNgoding/proton-clang.git --depth=1
 fi
 
