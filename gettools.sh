fb5c0f1cbcbad86b3699170978335f8f4f7f4098
MumetNgoding <nugroho31.com@gmail.com>
2020-11-10 23:06:52 +0700

    Script : Update Build Script

--- gettools.sh
+++ gettools.sh
@@ -9,14 +9,11 @@ git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarc
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r40 --depth=1 stock_32
 
 # Clone AnyKernel3
-git clone https://github.com/Risti699/AnyKernel3 -b master
+git clone https://github.com/MumetNgoding/BrynKernel-AOSP -b Memeui --depth=1
 
 #Download Clang
 if [ ! -d clang ]; then
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r383902c.tar.gz
-    mkdir -p clang/clang-r383902c
-    tar xvzf clang-r383902c.tar.gz -C clang/clang-r383902c
-    rm clang-r383902c.tar.gz
+git clone https://github.com/silont-project/silont-clang.git --depth=1
 fi
 
 # Download libufdt
