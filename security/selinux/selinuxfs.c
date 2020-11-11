95f80946c6076769845c1d7b47a9b04f37a619e7
MumetNgoding <nugroho31.com@gmail.com>
2020-11-12 01:13:39 +0700

    Selinux : Back Set to Enforcing this commit based from https://github.com/MumetNgoding/BrynKernel-AOSP/commit/da7dd39a3bae973b96e0c18851c20f0392fc2c70 now kernel set to Enforcing, because for Security Improvement

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -164,7 +164,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 		goto out;
 
 	if (new_value != selinux_enforcing) {
-		new_value = 0;
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
 			goto out;
